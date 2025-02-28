f24e3204332b3ed95ca7f7ad28ca692bee407d65
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2025-02-28 15:12:56 -0500

    SwiftUI Setup

---
+++ Flashify/FlashifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashifyApp.swift
+//  Flashify
+//
+//  Created by Ky Duyen on 28/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
